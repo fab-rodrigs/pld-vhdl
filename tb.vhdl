06f8826bee182dd981c32c8e778b0881d42c1f2a
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2026-03-10 20:02:40 -0300

    Add process to increment datab_tb
    Added a new process to increment datab_tb every 3 ns.

--- tb.vhdl
+++ tb.vhdl
@@ -20,6 +20,44 @@ begin
     process is
     begin
         for i in 0 to 234 loop
+            dataa_tb <= to_unsigned(i, dataa_tb'length);
+            wait for 5 ns;
+        end loop;
+        --data_tb <= x"0004";
+        --wait for 10 ns;
+        --dataa_tb <= x"0005";
+        --wait for 10 ns;
+    end process;
+
+    process is
+    begin
+        datab_tb <= datab_tb + 1;
+        wait for 3 ns;
+    end process       
+
+end architecture RTL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb is
+end entity tb;
+
+architecture RTL of tb is
+    signal dataa_tb : unsigned(3 downto 0) := (others => '0');
+    signal datab_tb : unsigned(3 downto 0) := (others => '0');
+    signal sum_tb : unsigned(3 downto 0);
+begin
+    
+    adder_inst : entity work.adder
+        port map(
+            dataa   => dataa_tb,
+            datab   => datab_tb,
+            sum => sum_tb
+        );
+    process is
+    begin
+        for i in 0 to 234 loop
             dataa_tb <= to_unsigned(i, dataa_tb'length);m
             wait for 5 ns;
         end loop;
